625c052893d868e31bfbb7e10a1d8b1ba8505bbb
tirth <techni621@gmail.com>
2020-05-26 21:27:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lyricsfinder/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.lyricsfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DownloadManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText authorName, songName;
+    private Button find;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        authorName = findViewById(R.id.AUTHOR_NAME);
+        songName = findViewById(R.id.SONG_NAME);
+        find = findViewById(R.id.FIND);
+        result = findViewById(R.id.RESULT);
+
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://api.lyrics.ovh/v1/" + authorName.getText().toString() + "/"
+                        + songName.getText().toString();
+                url.replace(" ", "20%");
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            result.setText(response.getString("lyrics"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        
+                    }
+                });
+
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+    }
+}
